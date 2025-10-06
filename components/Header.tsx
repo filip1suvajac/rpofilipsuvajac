a2524d3bc35183ef82e9cb95ef5af72d088b7b40
Filip Suvajac <filipsuvajac@Filips-MacBook-Air.local>
2025-10-06 13:16:46 +0200

    Commit, potreben za git stash predstavitev

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,8 @@ const Header: React.FC = () => {
     <header className="bg-blue-600 text-white py-6 shadow-md">
       <div className="container mx-auto px-4">
         <h1 className="text-3xl font-bold">Razvoj programske opreme</h1>
+        <p className="text-xl font-medium">To je sprememba, ki jo potrebujem za <span className="italic">git stash</span></p>
+        <p>To pa je čisto drugi paragraf</p>
       </div>
     </header>
   );
